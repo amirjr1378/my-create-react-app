19113d2703d06f15e780b0ef6a0656f8e8a7a798
amirjr <jramir669@gmail.com>
2019-07-25 16:29:00 +0430

    adding render method

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
-import React from "react"
-import withThanos from './ThanosEffect'
+import React from "react";
+import { withThanos, Thanos } from "./ThanosEffect";
 function App(props) {
-  const Thanos = withThanos(
-    function () { return <div><img src="https://www.sideshow.com/product-asset/903429/feature" alt="thanos" width='50' height='50' style={{ display: 'block' }} />should be disapeare</div> }
-  )
+  // with hoc api
+  const Thanoswith = withThanos(function() {
+    return (
+      <div>
+        <img
+          src="https://www.sideshow.com/product-asset/903429/feature"
+          alt="thanos"
+          width="50"
+          height="50"
+          style={{ display: "block" }}
+        />
+        should be disapeare
+      </div>
+    );
+  });
+  //with render props api
+  const RenderProps = () => (
+    <Thanos>
+      {({ handleClick }) => (
+        <div>
+          should fade when you click
+          <button onClick={() => handleClick()}>fade</button>
+        </div>
+      )}
+    </Thanos>
+  );
+
   return (
     <div>
-      <Thanos />
+      <Thanoswith />
+      <RenderProps />
     </div>
   );
 }
--- src/ThanosEffect.js
+++ src/ThanosEffect.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import { disintegrate } from './thanos.js'
-import "./thanos.scss"
-import { callbackify } from 'util';
-export default function (Component, callback) {
+import React from "react";
+import { disintegrate } from "./thanos.js";
+import "./thanos.scss";
+
+export function withThanos(Component, callback) {
+  const [show, setShow] = React.useState(true);
+  const divRef = React.createRef();
+  console.log(typeof Component);
+  function handleClick(event) {
+    disintegrate(divRef.current);
+    setTimeout(() => {
+      setShow(false);
+      callback && callback();
+    }, 1000);
+  }
+  function WrapperComponent() {
+    return (
+      <div ref={divRef} onClick={handleClick}>
+        <Component />
+      </div>
+    );
+  }
+
+  if (show) return WrapperComponent;
+  else return () => <span />;
+}
+
+export function Thanos({ children }) {
   const [show, setShow] = React.useState(true);
   const divRef = React.createRef();
 
-  function handleClick(event) {
-    disintegrate(divRef.current)
-    setTimeout(() => {
-      setShow(false);
-    }, 1000);
-    callback && callback()
+  function getStateAndHelpers() {
+    return {
+      handleClick: function(callback) {
+        disintegrate(divRef.current);
+        setTimeout(() => {
+          setShow(false);
+          callback && callback();
+        }, 1000);
+      },
+      show,
+      setShow
+    };
   }
-  function WrapperComponent() {
-    return <div ref={divRef} onClick={handleClick}>
-      <Component />
-    </div>
-  }
-
-
-  if (show)
-    return WrapperComponent
-  else return () => <span></span>
+  return <div ref={divRef}>{children(getStateAndHelpers())}</div>;
 }
